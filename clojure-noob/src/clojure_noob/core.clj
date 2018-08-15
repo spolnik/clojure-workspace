bfaaf4cdb76497363ab57ae36e62bb379b0dc024
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-08-15 07:20:41 +0200

    bit of more play with clojure

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -4,4 +4,8 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (println (if (= 0 1)
+             "omg 0 == 1!!"
+             "of course 0 <> 1"))
   (println "I'm a little teapot!"))
+
