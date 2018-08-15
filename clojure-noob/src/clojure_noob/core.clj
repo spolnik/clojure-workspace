cda49e3cd459e0f4c58968350a6e91f847b02105
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-08-15 07:33:01 +0200

    composing blocks of code^

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -5,7 +5,11 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println (if (= 0 1)
-             "omg 0 == 1!!"
-             "of course 0 <> 1"))
+             (do (println "Failure!")
+                 "omg 0 == 1!!")
+             (do (println "Success!!")
+                 "of course 0 <> 1")
+             ))
   (println "I'm a little teapot!"))
+
 
